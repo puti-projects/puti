e6b7dcfce48870285dec3525ad3a27992f1acb57
goozp <gzp@goozp.com>
2018-07-11 21:20:44 +0800

    add config

--- main.go
+++ main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"GdPHP/router"
+	"errors"
 	"log"
 	"net/http"
+	"time"
+
+	"gingob/config"
+	"gingob/router"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfg = pflag.StringP("config", "c", "", "gingob config file path.")
 )
 
 func main() {
+	pflag.Parse()
+
+	// init config
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// Set gin mode.
+	gin.SetMode(viper.GetString("runmode"))
+
 	// create the gin engine
 	g := gin.New()
 
@@ -18,7 +38,31 @@ func main() {
 	// routes
 	router.Load(g, middlewares...)
 
-	log.Printf("Start to listening the incoming requests on http address: %s", ":8000")
-	log.Printf(http.ListenAndServe(":8000", g).Error())
+	// Ping the server to make sure the router is working.
+	go func() {
+		if err := pingServer(); err != nil {
+			log.Fatal("The router has no response, or it might took too long to start up.", err)
+		}
+		log.Print("The router has been deployed successfully.")
+	}()
 
+	log.Printf("Start to listening the incoming requests on http address: %s", viper.GetString("addr"))
+	log.Printf(http.ListenAndServe(viper.GetString("addr"), g).Error())
+
+}
+
+// pingServer pings the http server to make sure the router is working.
+func pingServer() error {
+	for i := 0; i < viper.GetInt("max_ping_count"); i++ {
+		// Ping the server by sending a GET request to `/health`.
+		resp, err := http.Get(viper.GetString("url") + "/sd/health")
+		if err == nil && resp.StatusCode == 200 {
+			return nil
+		}
+
+		// Sleep for a second to continue the next ping.
+		log.Print("Waiting for the router, retry in 1 second.")
+		time.Sleep(time.Second)
+	}
+	return errors.New("Cannot connect to the router.")
 }
